5284346286eb73909bc1c36f4179d2c653e9cdb9
David Ojeda <david.ojeda@gmail.com>
2019-09-17 10:39:07 +0200

    Fill more setup.py options, classifiers, etc.

--- quetzal/openapi_client/__init__.py
+++ quetzal/openapi_client/__init__.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.1.0"
+__version__ = "0.3.1"
 
 # import apis into sdk package
 from quetzal.openapi_client.api.authentication_api import AuthenticationApi
